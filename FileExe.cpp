ff6f65d08cfd1c21f9313cb0866d8a59c6f6c37a
Sang Min Park <psm9733@naver.com>
2016-09-04 19:44:29 +0900

    Update FileExe.cpp

--- FileExe.cpp
+++ FileExe.cpp
@@ -1,23 +1,87 @@
 #include <iostream>
+#include <list>
+
 using namespace std;
+
+class File_state {
+private:
+	FILE *Faddress;
+	bool State;
+public:
+	FILE* put_file(FILE *file) {
+		if (file != NULL) {
+			State = true;
+			return this->Faddress;
+		}
+		State = false;
+	}
+
+	bool get_state() {
+		return this->State;
+	}
+
+	FILE* get_file() {
+		return this->Faddress;
+	}
+
+};
+
 class FileExe {
 private:
 	int OpenFile_Number;
-	bool def;
+	bool state;
 public:
-	FileExe(): OpenFile_Number(0) {
-
+	FileExe() : OpenFile_Number(0) {
+		list<File_state> Flist;
 	}
-	FILE* fopen(char* Path, char* mode) {
-		FILE *temp = new FILE();
-		temp = fopen(Path, mode);
-		temp != NULL ? def = true : def = false;
+
+	FILE* Fopen(char* Path, char* mode) {
+		FILE *temp;
+		fopen_s(&temp, Path, mode);
+		if (temp != NULL) {
+			state = true;
+			OpenFile_Number += 1;
+		}else {
+			state = false;
+		}
 		return temp;
 	}
-	void File_State() {
-		def == true ? cout << "파일열수있음" << endl : cout << "파일열수없음" << endl;	
+
+	void Fcopy(FILE* target, FILE* copy) {
+		int ch;
+		while ((ch = fgetc(target)) != EOF) {
+			putc(ch, stdout);
+			fputc(ch, copy);
+		}
 	}
+
+	void Fstate() {
+		if (state == true) {
+			cout << "파일열수있음" << endl;
+		}
+		else {
+			cout << "파일열수없음" << endl;
+		}
+	}
+
 	void ShowInfo() {
 		cout << "OpenFile Number is " << OpenFile_Number << endl;
 	}
+
+	~FileExe() {
+
+	}
 };
+
+
+int main() {
+
+	FileExe *Filehead = new FileExe();
+	FILE *target = (Filehead->Fopen("C:\\Users\\psm97\\Desktop\\copy\\target.txt", "rt"));
+	FILE *copy = (Filehead->Fopen("C:\\Users\\psm97\\Desktop\\copy\\copy.txt", "wt"));
+	Filehead->Fcopy(target, copy);
+	Filehead->Fstate();
+	Filehead->ShowInfo();
+	return 0;
+}
+
