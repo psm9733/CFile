6e5869988172b7e4bdf4ff9088b46ef715e73164
Sang Min Park <psm9733@naver.com>
2016-09-07 17:11:37 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,9 @@
 
 int main() {
 	arraylist<int> *list = new arraylist<int>();
-	int data;
-	int ins_number;
-	cout << "몇개의 data를 저장 하시겠습니까? [ ]\b\b";
-	cin >> ins_number;
-	for(int i = 0; i < ins_number; i++){
-		cout << "저장하실 데이터를 입력해주세요 [ ]\b\b";
-		cin >> data;
-		list->LINSERT(data);
-	}
+	list->LINSERT();
 	list->LSHOW();
 	return 0;
 }
+
 
