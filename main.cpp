6008ce2a51d7eb6b5461e4c25595016c0bd5d83a
Sang Min Park <psm9733@naver.com>
2016-09-14 21:43:18 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,48 @@
 
 int main() {
 	arraylist<int> *list = new arraylist<int>();
+	int target;
 	int choice;
 	while(true) {
-		if (choice == 0) {
+		cout << "========== ArrayList ==========" << endl;
+		cout << "1번 INSERT" << endl;
+		cout << "2번 DELETE" << endl;
+		cout << "3번 SHOWINFO" << endl;
+		cout << "4번 SEARCH" << endl;
+		cout << "5번 EXIT" << endl;
+		cout << "option select : [ ]\b\b";
+		cin >> choice;
+		if (choice == 1) {
 			list->INSERT();
 		}
-		else if (choice == 1) {
+		else if (choice == 2) {
+			cout << "삭제할 데이터를 입력하세요 : ";
+			cin >> target;
+			
+			if (target == list->LDELETE(target)) {
+				cout << target << "을 삭제 완료하였습니다." << endl;
+			}
+			else if(list->LDELETE(target) == NULL){
+				cout << target << "이 존재하지 않습니다." << endl;
+			}
+		}
+		else if (choice == 3) {
 			list->LSHOWINFO();
-		}else if
+		}
+		else if (choice == 4) {
+			cout << "찾을 데이터를 입력하세요 : ";
+			cin >> target;
+			if (list->LSEARCH(target)) {
+				cout << target << "이 존재합니다." << endl;
+			}
+			else {
+				cout << target << "이 존재하지 않습니다." << endl;
+			}
+		}
+		else {
+			break;
+		}
+		
 	}
-	
 	return 0;
 }
